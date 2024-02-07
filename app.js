73a3d8e7bfba63b62eeb8c48fe0d32f578f70333
Clumsy_Cranberry_1 <mlsshrbst@gmail.com>
2024-02-07 22:08:50 +0200

    removed feature for user to manually start a new game

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ class Field {
   }
   // --------------------------------------------------
   // The generateField method creates a random game field with a specified width and height. It includes a hat, holes, and a starting position.
-  static generateField(width, height) {
+  static generateField = (width, height) => {
     // helper function to generate a row of characters for the width of the field
-    function generateFieldWidth() {
+    let generateFieldWidth = () => {
       let array = [];
       let i = 0;
       for (i; i < width; i++) {
@@ -30,7 +30,7 @@ class Field {
         array[randomIndex] = characters["hole"];
       }
       return array;
-    }
+    };
 
     // generate a "height" amount of rows using the generateFieldWidth() helper function
     let fieldArray = [];
@@ -50,28 +50,28 @@ class Field {
     fieldArray[startY][startX] = characters["pathCharacter"];
 
     return fieldArray;
-  }
+  };
   // --------------------------------------------------
   // The startGame method initiates the game by printing the initial field and prompting the user to choose a direction.
-  startGame() {
+  startGame = () => {
     process.stdout.write("\n" + "Can you find your hat?" + "\n\n");
     this.printField(this.field);
     process.stdout.write(
       "\n" + "Choose a direction from the following: w = 'up', a = 'left', s = 'down', d = 'right'" + "\n" + "(q = 'quit game')" + "\n"
     );
     this.getUserData();
-  }
+  };
   // --------------------------------------------------
   // The getUserData method listens for user input using the process.stdin.on event handler.
-  getUserData() {
+  getUserData = () => {
     process.stdin.on("data", (userInput) => {
       let input = userInput.toString().trim().toLowerCase();
       this.actionUserInput(input, this.field);
     });
-  }
+  };
   // --------------------------------------------------
   // The actionUserInput method processes the user's input, updating the current game field or game status.
-  actionUserInput(userInput) {
+  actionUserInput = (userInput) => {
     // Validate user input
     let validInputs = {
       w: "up",
@@ -79,9 +79,6 @@ class Field {
       a: "left",
       d: "right",
       q: "quit",
-      // after a game is won the user has options to start a new game
-      1: "start new game",
-      2: "quit"
     };
 
     if (!validInputs[userInput]) {
@@ -91,27 +88,23 @@ class Field {
     }
 
     // helper function to test the player's current position and update the field accordingly
-    function testUserInput(y, x, fieldArray) {
+    let testUserInput = (y, x, fieldArray) => {
       let currentEl = fieldArray[y][x];
 
       if (currentEl === characters["hat"]) {
         process.stdout.write("\n" + "\u001b[36m" + "Congratulations! You found your hat!" + "\u001b[0m" + "\n");
-        process.stdout.write("\n" + "Play again?" + "\n" + "(1) Yes" + "\n" + "(2) No");
+        process.exit();
       } else if (currentEl === characters["hole"]) {
-        process.stdout.write("\n" + "Oh nooooo! You fell into a hole! Game Over." + "\n");
-        process.stdout.write("\n" + "Play again?" + "\n" + "(1) Yes" + "\n" + "(2) No");
+        process.stdout.write("\n" + "\u001b[31m" + "Oh nooooo! You fell into a hole! Game Over." + "\u001b[0m" + "\n");
+        process.exit();
       } else {
         fieldArray[y][x] = characters["pathCharacter"];
-        for (let i = 0; i < fieldArray.length; i++) {
-          console.log(fieldArray[i].join(" "));
-        }
+        this.printField(fieldArray);
       }
+    };
 
-      return;
-    }
-
-    // the following code updates the field according to the user's input
-    // before we move the player, we must first check that the player is moving within the field range
+    // the following code updates the game field according to the user's input
+    // note: before we move the player, we first check that the player is moving within the field range
     // move player position to the left
     if (userInput === "a") {
       if (this.charXPosition <= 0) {
@@ -160,35 +153,23 @@ class Field {
       }
     }
 
-    // start a new game
-    if (userInput === "1") {
-      process.stdout.write("\n" + "Starting a new game!\n");
-      const newFieldArray = Field.generateField(10, 10);
-      this.field = newFieldArray;
-      this.charXPosition = 0;
-      this.charYPosition = 0;
-      this.startGame();
-      return;
-    }
-
     // quit game
-    if (userInput === "q" || userInput === "2") {
+    if (userInput === "q") {
       console.log("\n" + "OK! See you later!");
       process.exit();
     }
-
-  }
+  };
 
   // --------------------------------------------------
-  printField(fieldArray) {
+  printField = (fieldArray) => {
     for (let i = 0; i < fieldArray.length; i++) {
       console.log(fieldArray[i].join(" "));
     }
-  }
-} // END OF CLASS
+  };
+}
 
-let fieldArray = Field.generateField(10, 10);
+let myFieldArray = Field.generateField(10, 10);
 
-const myField = new Field(fieldArray);
+let myField = new Field(myFieldArray);
 
 myField.startGame();
